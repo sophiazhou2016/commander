a647440ebea3bff4acdf80381d72c85c7e4d2b9c
zhourongjing <zhourongjing@ppdai.com>
2020-01-22 21:22:26 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# commander
+node commander.js
---
+++ commander.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+var fs = require('fs'),
+    path = process.cwd();
+const version = require('./package').version;
+
+console.log('path:', path);
+
+var run = function(obj) {
+    if(obj[0] === '-v') {
+        console.log('version is ' + version);
+    }else if (obj[1] === '-h') {
+        console.log('help');
+    }else {
+        fs.readdir(path, function (err, files) {
+            if(err) {
+                console.log(err);
+            }else {
+                for(var i = 0; i < files.length; i ++) {
+                    console.log('循环：', i, files[i]);
+                }
+            }
+        });
+    }
+};
+console.log(process.argv);
+run(process.argv.slice(2));
