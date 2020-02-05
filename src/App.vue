4f28f55bae97e434fba5ea729b22dc786714b132
zhourongjing <zhourongjing@ppdai.com>
2020-02-05 22:40:38 +0800

    调整项目结构

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,13 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <div id="nav">
+      <router-link to="/">Home</router-link>
+      | <router-link to="/home">home</router-link>
+      | <router-link to="/about">about</router-link>
+    </div>
     <router-view/>
   </div>
 </template>
-
-<script>
-export default {
-  name: 'App'
-}
-</script>
 
 <style>
 #app {
